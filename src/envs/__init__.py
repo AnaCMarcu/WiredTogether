156500957b58ee3e76e47cb7d56f1dded3402d3f
Ana Marcu <A.C.Marcu-1@student.tudelft.nl>
2026-02-18 12:41:41 +0100

    Update __init__.py

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,6 +1,6 @@
 """Environment wrappers for multi-agent Craftium."""
 
-from .openworld_parallel import OpenWorldMultiAgentEnv
+from .openworld_multi_agents import OpenWorldMultiAgentEnv
 from .openworld_roles_mvp import RoleBasedOpenWorld
 
 __all__ = [
