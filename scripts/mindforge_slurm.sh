eb368dc996f670f19793cec45a9e9a0f93a565d6
Ana Marcu <A.C.Marcu-1@student.tudelft.nl>
2026-04-02 16:18:58 +0200

    Change script

--- scripts/mindforge_slurm.sh
+++ scripts/mindforge_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=mindforge
 #SBATCH --partition=gpu-a100
-#SBATCH --time=02:00:00
+#SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus-per-task=1
