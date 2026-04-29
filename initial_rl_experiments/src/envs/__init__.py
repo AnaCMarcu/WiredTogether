1aea9a3d468158567987c6bc1f84c9e72873942c
Ana Marcu <A.C.Marcu-1@student.tudelft.nl>
2026-04-29 14:43:18 +0200

    Delete old files

--- initial_rl_experiments/src/envs/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Environment wrappers for multi-agent Craftium."""
-
-from .openworld_multi_agents import OpenWorldMultiAgentEnv
-from .openworld_roles_mvp import RoleBasedOpenWorld
-
-__all__ = [
-    "OpenWorldMultiAgentEnv",
-    "RoleBasedOpenWorld",
-]
--- initial_rl_experiments/src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Agent architectures for multi-agent RL."""
-
-from .agent import IC3NetLLMAgent, CommState, MESSAGES, MAPPOLSTMAgent
-
-__all__ = ["IC3NetLLMAgent", "CommState", "MESSAGES", "MAPPOLSTMAgent"]
